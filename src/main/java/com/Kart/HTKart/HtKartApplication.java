b0ac41df4bf78f584edef9c3898eafc52fbba332
Trivesh <triveshshivpatel@gmail.com>
2023-08-05 16:37:17 +0530

    Init app.

---
+++ src/main/java/com/Kart/HTKart/HtKartApplication.java
@@ -0,0 +1,13 @@
+package com.Kart.HTKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kart/HTKart/HtKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kart.HTKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
